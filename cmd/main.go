c42bebb8ff6a47041b28fdff3a1cf7c90ebab7d6
Maycon Vyctor <mayconsantosfc11@gmail.com>
2025-04-18 15:22:31 -0300

    Added the GET function to the API, and connected to the information set in the Postgres database

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"go-api/controler"
+	"go-api/db"
+	"go-api/repository"
+	"go-api/usecase"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,11 +13,24 @@ func main() {
 
 	server := gin.Default()
 
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+	//Camada de repository
+	ProductRepository := repository.NewProductRepository(dbConnection)
+	//Camada usecase
+	ProductUsecase := usecase.NewProductUsecase(ProductRepository)
+	//Camada de controls
+	ProductControler := controler.NewProductControler(ProductUsecase)
+
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	server.GET("/products", ProductControler.GetProducts)
 
 	server.Run(":8000")
 }
